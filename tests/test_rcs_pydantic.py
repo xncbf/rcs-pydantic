69ec3862a95b784bf95725f77a602e69541e3795
Francis <francis.kim@travelflan.com>
2022-01-21 17:11:48 +0900

    0.1.8

--- tests/test_rcs_pydantic.py
+++ tests/test_rcs_pydantic.py
@@ -1,15 +1,10 @@
 from faker import Faker
 
-from rcs_pydantic import __version__
 from rcs_pydantic.main import RcsMessage
 
 from . import factory
 
 fake = Faker()
-
-
-def test_version():
-    assert __version__ == "0.1.7"
 
 
 def test_rcs_message():
